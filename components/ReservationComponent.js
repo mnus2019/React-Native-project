37db4e514cf0f7800bf4011cc5b0269c0a7b8a1b
mnus2019 <tontoas2018@gmail.com>
2020-08-12 09:07:47 -0800

    Modal

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
-import {
-  Text,
-  View,
-  ScrollView,
-  StyleSheet,
-  Picker,
-  Switch,
-  Button,
-} from "react-native";
+import { Text, View, ScrollView, StyleSheet,
+  Picker, Switch, Button, Modal } from 'react-native';
 import DatePicker from "react-native-datepicker";
 
 class Reservation extends Component {
@@ -17,9 +10,14 @@ class Reservation extends Component {
     this.state = {
       campers: 1,
       hikeIn: false,
-      date: "",
-    };
+      date: '',
+      showModal: false
+  };
   }
+
+  toggleModal() {
+    this.setState({showModal: !this.state.showModal});
+}
 
   static navigationOptions = {
     title: "Reserve Campsite",
@@ -27,12 +25,17 @@ class Reservation extends Component {
 
   handleReservation() {
     console.log(JSON.stringify(this.state));
+    this.toggleModal();
+}
+
+resetForm() {
     this.setState({
-      campers: 1,
-      hikeIn: false,
-      date: "",
+        campers: 1,
+        hikeIn: false,
+        date: '',
+        showModal: false
     });
-  }
+}
 
   render() {
     return (
@@ -96,6 +99,27 @@ class Reservation extends Component {
             accessibilityLabel="Tap me to search for available campsites to reserve"
           />
         </View>
+        
+        <Modal
+                    animationType={'slide'}
+                    transparent={false}
+                    visible={this.state.showModal}
+                    onRequestClose={() => this.toggleModal()}>
+                    <View style={styles.modal}>
+                        <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
+                        <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text>
+                        <Text style={styles.modalText}>Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}</Text>
+                        <Text style={styles.modalText}>Date: {this.state.date}</Text>
+                        <Button
+                            onPress={() => {
+                                this.toggleModal();
+                                this.resetForm();
+                            }}
+                            color='#5637DD'
+                            title='Close'
+                        />
+                    </View>
+                </Modal>
       </ScrollView>
     );
   }
@@ -116,6 +140,22 @@ const styles = StyleSheet.create({
   formItem: {
     flex: 1,
   },
+  modal: { 
+    justifyContent: 'center',
+    margin: 20
+},
+modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    backgroundColor: '#5637DD',
+    textAlign: 'center',
+    color: '#fff',
+    marginBottom: 20
+},
+modalText: {
+    fontSize: 18,
+    margin: 10
+}
 });
 
 export default Reservation;
